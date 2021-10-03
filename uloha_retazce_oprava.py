6eef099dcff3ff4661ec1b7185d3ef565ba06f0b
David <78915771+davidj1337@users.noreply.github.com>
2021-10-03 20:10:11 +0200

    Add files via upload

---
+++ uloha_retazce_oprava.py
@@ -0,0 +1,23 @@
+def pocet_suborov():
+    while True:
+        odpoved = input("Zadaj počet súborov: ")
+        try:
+            return int(odpoved)
+        except ValueError:
+            print("To nebolo číslo!")
+
+
+
+input2 = []
+poradie_slova = 0
+
+with open("basnicka.txt", encoding="utf-8") as subor:
+    for riadok in subor:
+        input2 += riadok.split()
+
+for i in range(pocet_suborov()):
+    poradie_slova += 1
+    if poradie_slova == len(input2):
+        poradie_slova -= len(input2)
+    with open(f"""slovo{poradie_slova}""", mode="w", encoding="utf-8") as subor:
+        print(input2[poradie_slova - 1], file=subor)
