069c23dba80ed72a9f7a1a6106ab204059e392f5
Dávid Ježik <78915771+davidj1337@users.noreply.github.com>
2022-04-06 08:58:52 +0200

    Update asteroidos.py

--- asteroidos.py
+++ asteroidos.py
@@ -1,2 +1,3 @@
 https://ctrlv.sk/xjTd naprazdno
 https://ctrlv.sk/kUCx nakratko
+cloud6c.edupage.org/cloud/Elektrotechnicke_meranie_I_-_teoria_1_.pdf?z%3A0ZNOLkzLAhrrkTD1mvIqlV5%2BZ2p%2F%2BxFzRFE%2FztOZVnWj1CpnmjFOM%2BzuIXU7AahL
